8f142a6eec009e09e989949cffc414f12b02f6c7
Furhan Samani <contact@furhan.dev>
2022-04-26 15:11:54 -0700

    get timer working along with play again button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,22 @@
-var viewHighscoresLink;
 var sections;
-var current;
-var previous;
-var backButton;
-var startButton;
-var timer;
+var state;
+var timeLeft;
+var viewHighscoresButtonEl;
+var playAgainButtonEl;
+var startButtonEl;
+var timerEl;
 
 /**
  * Hide all sections
  */
 function hideAllSections() {
-    for (let i = 0; i < sections.length; i++) {
-        hide("#" + sections[i]);
-    }
+    // for (let i = 0; i < sections.length; i++) {
+    //     hide("#" + sections[i]);
+    // }
+
+    sections.forEach(section => {
+        hide("#" + section);
+    });
 }
 
 /**
@@ -36,35 +40,64 @@ function hide(selector) {
 }
 
 /**
- * Render a section, hide all others
+ * Render a section, hide all others using 'display: none'
  * @param {string} section name of section to render
  */
 function renderSection(section) {
-    if (current === section) {
+
+    if (state === section) {
         return;
     }
 
     // hide everything else
     hideAllSections();
-    // also hide timer if user isn't in the quiz section
+
+    // hide timer unless section is quiz
     if (section !== "quiz") {
-        hide(".timer");
+        timerEl.hidden = true;
+    }
+
+    // disable highscores button when section is highscores
+    if (section === "highscores") {
+        viewHighscoresButtonEl.disabled = true;
+    } else {
+        viewHighscoresButtonEl.disabled = false;
     }
 
     // update the state
-    previous = current;
-    current = section;
+    state = section;
 
+    // finally unhide the section we want to render
     unhide("#" + section);
 }
 
 /**
- * Render/unhide any element, given the selector 
+ * Render the timer based on how much time is left
+ */
+function renderTimeLeft() {
+    if (timeLeft < 10) {
+        timerEl.setAttribute("class", "timer alert alert-danger");
+    } else {
+        timerEl.setAttribute("class", "timer alert alert-primary");
+    }
+    
+    timerEl.hidden = false;
+    timerEl.textContent = "Timer: " + timeLeft + " seconds";
+}
+
+/**
+ * Render/unhide any element, given the selector
+ * Removes the 'display: none' style from element
  * @param {string} selector element, classname, or id
  */
 function unhide(selector) {
     let element = document.querySelector(selector);
     let classAttr = element.getAttribute("class");
+
+    // only unhide if hidden
+    if (!classAttr.includes("hidden")) {
+        return;
+    }
 
     let split = classAttr.split("hidden");
     // remove "hidden" from class
@@ -72,16 +105,36 @@ function unhide(selector) {
     element.setAttribute("class", classAttr);
 }
 
+/**
+ * Callback function for 'Start Quiz' and 'Play Again' buttons
+ * Starts timer, displays questions, validates answers, keeps score
+ */
 function startQuiz() {
     renderSection("quiz");
-    unhide(".timer");
-    timer = 30;
-    
+    timeLeft = 30;
+    renderTimeLeft();
+
     // set time interval
+    let timerInterval = setInterval(function () {
+        timeLeft--;
 
-    // update timer element color when 10 seconds left
+        // when time hits zero, show done section, otherwise render time
+        if (timeLeft === 0) {
+            renderSection("done");
+        }
 
-    // end quiz if time runs out or no more questions left
+        // stop timer interval if not in quiz or timeLeft is zero
+        if (state !== "quiz" || timeLeft === 0) {
+            clearInterval(timerInterval);
+        } else {
+            renderTimeLeft();
+        } 
+    }, 1000);
+
+    let moreQuestions = false;
+    while (moreQuestions && timeLeft > 0) {
+        // Serve questions
+    }
 }
 
 /**
@@ -90,31 +143,21 @@ function startQuiz() {
 function init() {
 
     // select elements
-    viewHighscoresLink =  document.querySelector(".view-highscores");
-    startButton = document.querySelector(".start-button");
-    backButton = document.querySelector(".back");
+    viewHighscoresButtonEl =  document.querySelector(".view-highscores");
+    startButtonEl = document.querySelector(".start-button");
+    playAgainButtonEl = document.querySelector(".play-again");
+    timerEl = document.querySelector(".timer");
+    // hide timmer
+    timerEl.hidden = true;
 
-    // init quiz state
-    current = "start";
-    previous = "start"; 
-    sections = ["start", "quiz", "done", "highscores"];
-    timer = 0;
+    sections = ["welcome", "quiz", "done", "highscores"];
 
     // add listeners
-    viewHighscoresLink.addEventListener("click", function(event) {
+    viewHighscoresButtonEl.addEventListener("click", function(event) {
         renderSection("highscores");
     });
-
-    backButton.addEventListener("click", function(event) {
-        // don't allow user to go back to quiz
-        if (previous === "quiz") {
-            renderSection("start");
-        } else {
-            renderSection(previous);
-        }
-    });
-
-    startButton.addEventListener("click", startQuiz);
+    playAgainButtonEl.addEventListener("click", startQuiz);
+    startButtonEl.addEventListener("click", startQuiz);
 }
 
 init();
